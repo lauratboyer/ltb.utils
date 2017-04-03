ea2085dc254fb94df4af5f4f4a6182a20b3c7da6
Laura Tremblay-Boyer <laura.t.boyer@gmail.com>
2017-04-03 16:55:10 +1100

    edits to doitall4cyg improving doc and adding more options (carriadge change, header fix, etc.)

--- R/doitall4cygwin.r
+++ R/doitall4cygwin.r
@@ -1,15 +1,38 @@
-#' doitall4cygwin.r
+# doitall4cygwin.r
 ####################################################
-#' doitall4cyg() converts a windows-exec doitall to a unix version
+#' Conversion of MFCL doitall from DOS to Unix format
+#'
+#' Converts a windows-exec doitall to a unix version, saved as input file with extension .cyg;
+#' also does carriage switch from Dos to Unix format
 #' @param x file name for doitall to convert
+#' @param keep.condor if FALSE (the default), will remove the 3 CONDOR header lines if present
 #' @keywords mfcl
-#' @export
 #' @author Laura Tremblay-Boyer (lauratb@spc.int)
-#' doitall4cyg()
-doitall4cyg <- function(x) {
+#' @export
+doitall4cyg <- function(x, keep.condor=FALSE) {
     v0 <- scan(x, 'character', sep='\n')
-    v1 <- v0[-(1:4)]
-    v2 <- gsub('nice \\$MFCL', './mfclo64', v1)
-    cat(v2, file=sprintf('%s.cyg', x, sep='\n'), sep='\n')
+    v2 <- gsub('nice \\$MFCL', './mfclo64', v0)
+    fname <- sprintf('%s.cyg', x)
+    if(!any(grepl('#!/bin/sh', v2))) {
+        v2 <- c('#!/bin/sh', v2)
+        message('Added first lines #!/bin/sh/n')}
+
+    ## Look for CONDOR header (3 lines)
+    cndr.check <- grepl("cd \\$_CONDOR_SCRATCH_DIR|export PATH=.:\\$PATH|export ADTMP1=.", v2)
+    if(any(cndr.check)){
+        message('--> CONDOR header present in file')
+        if(!keep.condor) { v2 <- v2[!cndr.check]; message(' ...CONDOR header removed\n') }
+    }
+    if(!any(cndr.check) & keep.condor) warning('\nCONDOR header missing from file')
+
+    ## Check for windows code ('$')
+    if(any(grepl('\\$', v2))) warning("Windows syntax still present in file, search for '$'")
+    cat(v2, file=fname, sep='\n')
+
+    ## could also use dos2unix
+    system(sprintf("sed -i 's/\r$//' %s", fname))
 }
 
+#cd $_CONDOR_SCRATCH_DIR
+#export PATH=.:$PATH
+#export ADTMP1=.
